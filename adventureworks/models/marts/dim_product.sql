6bdcb7e9a66f38b1892eccf692454816b49bef5f
jeremypayan <jeremy.payan17@gmail.com>
2023-10-25 14:59:45 +0200

    product dimension ok

--- adventureworks/models/marts/dim_product.sql
+++ adventureworks/models/marts/dim_product.sql
@@ -1,26 +1,26 @@
 WITH stg_product AS (
-    SELECT productid, name AS productname, modifieddate, productmodelid, standardcost, productsubcategoryid, sellstartdate
+    SELECT productid, name AS productname, modifieddate, productmodelid, standardcost, productsubcategoryid, sellstartdate, color
     FROM {{ ref('product') }}
 ),
 
-WITH stg_productcategory AS (
+stg_productcategory AS (
     SELECT productcategoryid, name AS productcategoryname, modifieddate
     FROM {{ ref('productcategory') }}
 ),
 
-WITH stg_productsubcategory AS (
+stg_productsubcategory AS (
     SELECT productcategoryid, productsubcategoryid, name AS productsubcategoryname, modifieddate
-    FROM {{ ref('productsubcategoryid') }}
+    FROM {{ ref('productsubcategory') }}
 )
 
 SELECT {{ dbt_utils.generate_surrogate_key(['stg_product.productid']) }} as product_key,
        stg_product.productid,
-       stg_product.productname,
+       stg_product.productname AS product_name,
        stg_product.color,
-       productcategoryname,
-       productsubcategoryname
+       stg_productcategory.productcategoryname,
+       stg_productsubcategory.productsubcategoryname
 FROM stg_product
-LEFT JOIN stg_productecatgory
+LEFT JOIN stg_productcategory
     ON stg_product.productmodelid = stg_productcategory.productcategoryid
 LEFT JOIN stg_productsubcategory
     ON stg_product.productsubcategoryid = stg_productsubcategory.productsubcategoryid
